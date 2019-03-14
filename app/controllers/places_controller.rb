741487ca158aea6b553d04516fe5756f81a94e63
PJToews <lohasjean@gmail.com>
2019-03-14 04:38:04 +0000

    Making the Submit Button Create a New Place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,20 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
 
-def index
-  @places = Place.all
-end
+  def new
+    @place = Place.new
+  end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
-def new
-  @place = Place.new
-end
+  private
 
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
