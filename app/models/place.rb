a4387303e6ab946a694e5551a9c7a2141b4b38c8
PJToews <lohasjean@gmail.com>
2019-03-22 01:56:34 +0000

    Adding Comments to the Web Application and Setting Up the Database and Models

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
+
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true, length: { maximum: 50, minimum: 3 }
