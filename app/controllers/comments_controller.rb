75732a65d718edb4d56219cffa0020a7731b004b
PJToews <lohasjean@gmail.com>
2019-03-27 18:28:29 +0000

    test image

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    # see the comments that the user inputted and is trying to save
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
